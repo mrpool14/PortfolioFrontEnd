d6a822b6b8cf27bb0f1c88489544c43d48eb9f9c
password <mentuccigil@hotmail.com>
2022-12-17 18:56:03 -0300

    nuevo commit

--- prueba.js
+++ prueba.js
@@ -2,4 +2,10 @@ console.log("hola mundo")
 console.warn(`peligro`)
 console.error(`error`)
 console.info(`info`)
+console.info(`info`)
+console.info(`info`)
+console.info(`info`)
+console.info(`info`)
+
+
 
